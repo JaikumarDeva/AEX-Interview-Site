356c0311f9c5c0f7a7f3b659fbcb554ea8c1c8d0
Jaikumar Devadoss <jaikumar.devadoss@whitebread.com>
2025-06-11 19:09:55 +0530

    Initial set up

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to React coding exercise
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
